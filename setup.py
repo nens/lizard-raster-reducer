496d4121e0eb57f8d6aaaf8a69db61f3359eaa0d
Reinout van Rees <reinout@vanrees.org>
2019-03-27 14:37:16 +0100

    Shortening pypi description

--- setup.py
+++ setup.py
@@ -18,15 +18,7 @@ tests_require = [
 setup(
     name="lizard-raster-reducer",
     version=version,
-    description="Lizard raster reducer is a tool to auto-generate regional reports from Lizard data. "
-    "It reduces rasters to aggregate statistics per region. "
-    "Multiple rasters can be specified. The first raster will act as the scope raster. "
-    "The scope raster determines the spatial extent and temporal behaviour of the result. "
-    "Rasters can be temporal or static. "
-    "Rasters can contain continuous values or discrete classes. "
-    "Regions of one region type are used for the result. "
-    "Regions within the spatial extent of the scope raster are used in the result. "
-    "A configuration file is used to customize the output.",
+    description="Lizard raster reducer is a tool to auto-generate regional reports from Lizard data.",
     long_description=long_description,
     # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=["Programming Language :: Python", "Framework :: Django"],
